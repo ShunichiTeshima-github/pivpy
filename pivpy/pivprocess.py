da6e85e3eea3019436fceeebc9db4aaa049e6b0e
ShunichiTeshima-github <shunichi.teshima@gmail.com>
2021-07-06 23:43:40 +0900

    add peak detection and suppix interp

--- pivpy/pivprocess.py
+++ pivpy/pivprocess.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+from scipy.ndimage import maximum_filter
 
 
 def _correlation_list(
@@ -45,12 +46,46 @@ def _correlation_map(window_img0, img1, interrogation_window, search_window, ind
 
 
 def _detect_peak(correlation_map):
+    def _sub_pixel_interpolation():
+        try:
+            value0 = max(correlation_map[peak_j, peak_i], 0.01)
+            value1 = max(correlation_map[peak_j - 1, peak_i], 0.01)
+            value2 = max(correlation_map[peak_j + 1, peak_i], 0.01)
+            value3 = max(correlation_map[peak_j, peak_i - 1], 0.01)
+            value4 = max(correlation_map[peak_j, peak_i + 1], 0.01)
+            delta_j = peak_j + 0.5 * (np.log(value1) - np.log(value2)) / (np.log(value1) + np.log(value2) - 2*np.log(value0))
+            delta_i = peak_i + 0.5 * (np.log(value3) - np.log(value4)) / (np.log(value3) + np.log(value4) - 2*np.log(value0))
+            if np.isnan(delta_j):
+                delta_j = peak_j
+            if np.isnan(delta_i):
+                delta_i = peak_i
+            return delta_j, delta_i
+        except IndexError:
+            return peak_j, peak_i
+
     correlation_map[np.isnan(correlation_map)] = 0
-    if np.sum(np.abs(correlation_map)) == 0:
-        return np.nan, np.nan
-    else:
+
+    local_max = maximum_filter(correlation_map, footprint=np.ones((5, 5)), mode='constant')
+    detected_peaks = np.ma.array(correlation_map, mask=~(correlation_map == local_max))
+    temp = np.ma.array(detected_peaks, mask=~(detected_peaks >= detected_peaks.max() * 0.3))
+    peaks_index = np.where((temp.mask == 0))
+
+    if len((peaks_index[0])) == 1:
         peak_j, peak_i = np.unravel_index(correlation_map.argmax(), correlation_map.shape)
-    return peak_j, peak_i
+        return _sub_pixel_interpolation()
+    elif len((peaks_index[0])) > 1:
+        peak_j, peak_i = np.unravel_index(correlation_map.argmax(), correlation_map.shape)
+        value_lis = []
+        for num in range(len((peaks_index[0]))):
+            value_lis.append(correlation_map[peaks_index[0][num], peaks_index[1][num]])
+        rs1 = sorted(value_lis)[-1]
+        rs2 = sorted(value_lis)[-2]
+        if rs1 / rs2 >= 1.75:
+            return _sub_pixel_interpolation()
+        else:
+            return np.nan, np.nan
+    else:
+        return np.nan, np.nan
 
 
 def normal_piv(
