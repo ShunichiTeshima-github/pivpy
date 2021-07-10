ef4be918cb29a6815026ea6e0b3d408b7b791bdf
ShunichiTeshima-github <shunichi.teshima@gmail.com>
2021-07-10 23:45:37 +0900

    add documentation

--- pivpy/preprocess.py
+++ pivpy/preprocess.py
@@ -4,12 +4,42 @@ import cv2
 
 
 def remove_background(img, background):
+    """remove_background(img, background)
+    Return image removed background
+
+    Parameters
+    ----------
+    img : ndarray
+        8bit input image, only 1Ch(gray) iamge
+    background : ndarray
+        8bit background image, only 1Ch(gray) iamge
+
+    Returns
+    -------
+    out : ndarray
+        8bit output image, only 1Ch(gray) iamge
+    """
     img_removed = np.where(img > background, img - background, 0)
     img_removed = img_removed.astype(np.uint8)
     return img_removed
 
 
 def high_pass_filter(img, ksize=9):
+    """high_pass_filter(img, ksize)
+    Return image removed low frequency infomation
+
+    Parameters
+    ----------
+    img : ndarray
+        8bit input image, only 1Ch(gray) iamge
+    ksize : int
+        kernel size for threshold of filter
+
+    Returns
+    -------
+    out : ndarray
+        8bit output image, only 1Ch(gray) iamge
+    """
     img_low_hz = cv2.medianBlur(img, ksize=ksize)
     img_removed = np.where(img > img_low_hz, img - img_low_hz, 0)
     img_removed = img_removed.astype(np.uint8)
